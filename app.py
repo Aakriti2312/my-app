5701c589316d79e3526c4754ea74146bdbca59c0
Aakriti Dhawan <dhawanaakriti2312@gmail.com>
2026-04-16 10:26:01 +0530

    final pipeline trigger

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask # type: ignore
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from CI/CD Pipeline!"
+    return "CI/CD is working 🚀 - Aakriti FINAL"
 
-app.run(host="0.0.0.0", port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
